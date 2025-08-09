315e1688c3be610ea6cb31394664c554cf0fd071
bokumentation <ibraheli41@gmail.com>
2025-08-09 11:19:09 +0700

    update minor
    added config for snack picker and some configuration to clangd

--- lua/plugins/clangd.lua
+++ lua/plugins/clangd.lua
@@ -30,12 +30,7 @@ return {
         },
         cmd = {
           "clangd",
-          "--background-index",
-          "--clang-tidy",
-          "--header-insertion=iwyu",
-          "--completion-style=detailed",
-          "--function-arg-placeholders",
-          "--fallback-style=llvm",
+          "query-driver=/usr/bin/g++",
         },
         init_options = {
           usePlaceholders = true,
